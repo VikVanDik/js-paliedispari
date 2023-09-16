40f7eb9a9c61d8e215579aaa9ccd89e18868944b
VikVanDik <95vlc17@gmail.com>
2023-09-16 16:15:47 +0200

    [add]: index and script files

---
+++ script/script.js
@@ -0,0 +1,15 @@
+
+// 1. Creare il prompt per ricevere l'input dall'utente.
+const input = prompt('inserisci una parola')
+console.log(input);
+
+
+// 2. Creare una funzione che prenda la stringa e la trasformi in un array, la inverta e la confronti con la stringa originale dopo averla di nuovo trasformata in una stringa.
+function palindrome (inputWord) {
+
+}
+
+
+// 3. Se le due stringhe confrontate sono uguali la parola è palindroma.
+// 4. Stampare il risultato 
+
