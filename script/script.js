6bac36cbfbec207324ed9d9152036e5e13ed5255
VikVanDik <95vlc17@gmail.com>
2023-09-16 16:24:47 +0200

    [add]: complete function

--- script/script.js
+++ script/script.js
@@ -6,10 +6,24 @@ console.log(input);
 
 // 2. Creare una funzione che prenda la stringa e la trasformi in un array, la inverta e la confronti con la stringa originale dopo averla di nuovo trasformata in una stringa.
 function palindrome (inputWord) {
-
+  const wordArr = inputWord.split("")
+  console.log(wordArr);
+  const wordArrInv = wordArr.reverse()
+  console.log(wordArrInv);
+  const wordInv = wordArrInv.join("")
+  console.log(wordInv);
+  
+  // 3. Se le due stringhe confrontate sono uguali la parola è palindroma.
+  if (inputWord === wordInv) {
+    // 4. Stampare il risultato 
+    console.log('La parola è palindroma');
+  } else {
+    // 4. Stampare il risultato 
+    console.log('La parola non è palindroma');
+  }
 }
 
+palindrome (input)
 
-// 3. Se le due stringhe confrontate sono uguali la parola è palindroma.
-// 4. Stampare il risultato 
+
 
