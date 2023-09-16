8413a963e852a1468c843d8e8ecc1a27755cf2af
VikVanDik <95vlc17@gmail.com>
2023-09-16 17:12:44 +0200

    [add] : completed logic in console

--- script/script2.js
+++ script/script2.js
@@ -1,11 +1,24 @@
-// 1. Creare il prompt per ricevere un numero da 1 a 5 dall'utente
+// 1. Creare il prompt per ricevere un numero da 1 a 5 dall'utente e per la scelta di pari e dispari
+const ped = prompt('Pari o dispari?')
 const input = parseFloat(prompt('inserisci un numero da uno a cinque'))
+
+// 2. Verificare che l'utente abbia inserito pari o dispari
+if (ped === 'pari') {
+  console.log('Hai scelto pari');
+}else if (ped === 'dispari') {
+  console.log('Hai scelto dispari');
+}else {
+  alert ('Devi scegliere pari o dispari')
+  location.reload()
+}
 
 // 2. Verificare che sia un numero e che sia compreso tra i due valori indicati
 if (isNaN(input)){
-  console.log('Devi inserire un numero');
+  alert ('Devi inserire un numero');
+  location.reload()
 } else if (input < 1 || input > 5) {
-  console.log("Il numero dev'essere compreso tra 1 e 5");
+  alert ("Il numero dev'essere compreso tra 1 e 5");
+  location.reload()
 } else {
   console.log(input);
 }
@@ -25,13 +38,26 @@ console.log(sum);
 function isEven (num) {
   if (num % 2 === 0) {
     console.log('Il numero è pari');
+    // 6. Dichiarare il vincitore.
+    if (ped === 'pari') {
+      // 7. Stampare il risultato.
+      console.log('Hai vinto');
+    }else {
+      // 7. Stampare il risultato.
+      console.log('Ritenta, sarai più fortunato');
+    }
   } else {
     console.log('Il numero è dispari');
+    // 6. Dichiarare il vincitore.
+    if (ped === 'dispari') {
+      // 7. Stampare il risultato.
+      console.log('Hai vinto');
+    }else {
+      // 7. Stampare il risultato.
+      console.log('Ritenta, sarai più fortunato');
+    }
   }
 }
 
 isEven (sum)
-// 6. Dichiarare il vincitore.
 
-
-// 7. Stampare il risultato.
